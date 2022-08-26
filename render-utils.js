52a4216d103ed42001b8ced0ea897e5eb57e9ce0
Connor McCue <connor.mccue@comcast.net>
2022-08-25 23:09:07 -0700

    did my last commit work

--- render-utils.js
+++ render-utils.js
@@ -4,8 +4,11 @@ export function renderDemGroceries(groceries) {
         const li = document.createElement('li');
         li.textContent = food.quantity + ' ' + food.item;
         if (food.bought === false) {
+            // console.log('we in the if loop');
             li.addEventListener('click', async () => {
                 food.bought = true;
+                li.classList.add('purchased');
+                // console.log('you purchased ' + food.quantity + ' ' + food.item);
             });
         } else {
             li.classList.add('purchased');
